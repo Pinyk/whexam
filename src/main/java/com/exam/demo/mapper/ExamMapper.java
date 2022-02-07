6ab66303943221a2fc1b5be76fc94ac85798c211
sssouth <1182394023@qq.com>
2022-02-07 19:57:00 +0800

    试卷模块

--- src/main/java/com/exam/demo/mapper/ExamMapper.java
+++ src/main/java/com/exam/demo/mapper/ExamMapper.java
@@ -9,10 +9,25 @@ import java.util.List;
 @Repository
 public interface ExamMapper extends BaseMapper<Exam> {
 
+    /**
+     * 根据试卷ID查询组成试卷的判断题目
+     * @param testPaperId
+     * @return
+     */
     List<Object> findExamJudgeByTestPaperId(Integer testPaperId);
 
+    /**
+     * 根据试卷ID查询组成试卷的选择题目
+     * @param testPaperId
+     * @return
+     */
     List<Object> findExamSelectByTestPaperId(Integer testPaperId);
 
+    /**
+     * 根据试卷ID查询组成试卷的主观题目
+     * @param testPaperId
+     * @return
+     */
     List<Object> findExamSubjectByTestPaperId(Integer testPaperId);
 
 }
