ac4d89ce6514ded1dced2a24d11595e9bdf7c472
sssouth <1182394023@qq.com>
2022-02-09 17:13:30 +0800

    Merge branch 'master' of https://github.com/Pinyk/whexam into dev2

---
+++ src/main/java/com/exam/demo/mapper/StudyMapper.java
@@ -0,0 +1,17 @@
+package com.exam.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.exam.demo.entity.Study;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author: csx
+ * @Date: 2022/1/23
+ */
+@Repository
+public interface StudyMapper extends BaseMapper<Study> {
+
+
+
+
+}
