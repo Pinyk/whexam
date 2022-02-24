93e11e060f1b23cce3c71b8e59b44cbe9975def8
caoshuxinaaaaa <499908355@qq.com>
2022-02-24 12:28:16 +0800

    Merge branch 'master' of github.com:Pinyk/whexam into dev3
     Conflicts:
    	src/main/resources/application.properties

--- src/main/java/com/exam/demo/controller/StudyController.java
+++ src/main/java/com/exam/demo/controller/StudyController.java
@@ -1,15 +1,21 @@
 package com.exam.demo.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.exam.demo.entity.Study;
 import com.exam.demo.service.StudyService;
+import com.exam.demo.utils.Consts;
 import com.exam.demo.utils.WebResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import static com.exam.demo.utils.WebResult.REQUEST_STATUS_SUCCESS;
@@ -77,28 +83,28 @@ public class StudyController {
 
     }
 
-    //增加新的学习任务
-    @PostMapping("/insert")
-    @ApiOperation(notes = "csx",value = "插入课程接口")
-    public WebResult<Integer> insert(@RequestParam @ApiParam(name="id") Integer id,
-                      @RequestParam @ApiParam(name="name") String name,
-                      @RequestParam @ApiParam(name="datatype_id") Integer datatype_id,
-                      @RequestParam @ApiParam(name="url") String url,
-                      @RequestParam @ApiParam(name= "subject_id",defaultValue = "0") Integer subject_id,
-                      @RequestParam @ApiParam(name="department_id") Integer departement_id){
-        Study study=new Study();
-        study.setId(id);
-        study.setName(name);
-        study.setDepartment_id(datatype_id);
-        study.setUrl(url);
-        study.setSubject_id(subject_id);
-        study.setDatatype_id(datatype_id);
-        return WebResult.<Integer>builder()
-                .code(200)
-                .message(REQUEST_STATUS_SUCCESS)
-                .data(studyService.insert(study))
-                .build();
-    }
+//    //增加新的学习任务
+//    @PostMapping("/insert")
+//    @ApiOperation(notes = "csx",value = "插入课程接口")
+//    public WebResult<Integer> insert(@RequestParam @ApiParam(name="id") Integer id,
+//                      @RequestParam @ApiParam(name="name") String name,
+//                      @RequestParam @ApiParam(name="datatype_id") Integer datatype_id,
+//                      @RequestParam @ApiParam(name="url") String url,
+//                      @RequestParam @ApiParam(name= "subject_id",defaultValue = "0") Integer subject_id,
+//                      @RequestParam @ApiParam(name="department_id") Integer departement_id){
+//        Study study=new Study();
+//        study.setId(id);
+//        study.setName(name);
+//        study.setDepartment_id(datatype_id);
+//        study.setUrl(url);
+//        study.setSubject_id(subject_id);
+//        study.setDatatype_id(datatype_id);
+//        return WebResult.<Integer>builder()
+//                .code(200)
+//                .message(REQUEST_STATUS_SUCCESS)
+//                .data(studyService.insert(study))
+//                .build();
+//    }
 
     //修改学习任务
     @PostMapping("/updata")
@@ -123,6 +129,70 @@ public class StudyController {
                 .data(studyService.update(study))
                 .build();
 
+    }
+//    添加学习资料
+
+
+    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    @ApiOperation(notes = "csx",value = "插入课程接口")
+    public Object addSong(HttpServletRequest request, @RequestParam("file") MultipartFile mpFile){
+        JSONObject jsonObject = new JSONObject();
+        //获取前端传来的参数
+        int id = Integer.parseInt(request.getParameter("StudyId").trim());  //学习资料ID
+        String name = request.getParameter("name").trim();    //学习资料名称
+        int datatype_id = Integer.parseInt(request.getParameter("datatype_id").trim());          //所属学习类型
+        String url = "/img/songPic/tubiao.jpg";                     //url
+        int subject_id = Integer.parseInt(request.getParameter("subject_id").trim());     //所属学科id
+        int department_id = Integer.parseInt(request.getParameter("department_id").trim());     //所属资料部门id
+        //上传歌曲文件
+        if(mpFile.isEmpty()){
+            jsonObject.put(Consts.CODE,0);
+            jsonObject.put(Consts.MSG,"歌曲上传失败");
+            return jsonObject;
+        }
+        //文件名=当前时间到毫秒+原来的文件名
+        String fileName = System.currentTimeMillis()+mpFile.getOriginalFilename();
+        //文件路径
+        String filePath = System.getProperty("user.dir")+System.getProperty("file.separator")+"study";
+        //如果文件路径不存在，新增该路径
+        File file1 = new File(filePath);
+        if(!file1.exists()){
+            file1.mkdir();
+        }
+        //实际的文件地址
+        File dest = new File(filePath+System.getProperty("file.separator")+fileName);
+        //存储到数据库里的相对文件地址
+        String storeUrlPath = "/song/"+fileName;
+        try {
+            mpFile.transferTo(dest);
+            Study study=new Study();
+            study.setId(id);
+            study.setName(name);
+            study.setDepartment_id(datatype_id);
+            study.setUrl(url);
+            study.setSubject_id(subject_id);
+            study.setDatatype_id(datatype_id);
+            int flag = studyService.insert(study);
+            if(flag==1){
+//                jsonObject.put(Consts.CODE,1);
+//                jsonObject.put(Consts.MSG,"保存成功");
+//                jsonObject.put("avator",storeUrlPath);
+//                return jsonObject;
+
+                return WebResult.<Integer>builder()
+                        .code(200)
+                        .message(REQUEST_STATUS_SUCCESS)
+                        .build();
+            }
+            jsonObject.put(Consts.CODE,0);
+            jsonObject.put(Consts.MSG,"保存失败");
+            return jsonObject;
+        } catch (IOException e) {
+            jsonObject.put(Consts.CODE,0);
+            jsonObject.put(Consts.MSG,"保存失败"+e.getMessage());
+        }finally {
+            return jsonObject;
+        }
     }
 
 
