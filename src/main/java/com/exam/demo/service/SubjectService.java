0b866035142b2312ed493919f9e12e196999a755
Pinyk <1748708966@qq.com>
2022-03-01 13:10:16 +0800

    增加返回所有部门 学科接口

--- src/main/java/com/exam/demo/service/SubjectService.java
+++ src/main/java/com/exam/demo/service/SubjectService.java
@@ -2,6 +2,8 @@ package com.exam.demo.service;
 
 import com.exam.demo.entity.Subject;
 
+import java.util.List;
+
 /**
  * @Author: csx
  * @Date: 2022/2/28
@@ -11,4 +13,11 @@ public interface SubjectService {
 
     //根据id查科目
     Subject findById(int id);
+
+
+    /**
+     * 返回所有学科
+     * @return
+     */
+    List<Subject> findAll();
 }
